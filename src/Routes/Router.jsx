a5e623a23c6069a86f6209c86ff97af5faf9026d
saiful797 <saifulislamsaif797@gmail.com>
2024-05-31 20:43:33 +0600

    Basic setup done successfully!

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/HomePage/Home/Home";
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      errorElement: '',
+      children: [
+        {
+            path: '/',
+            element: <Home />
+        }
+      ]
+    },
+  ]);
