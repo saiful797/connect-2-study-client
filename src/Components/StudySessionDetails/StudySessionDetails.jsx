d3f16fb517b79a4b2708fde11b1d7b9df5fe0fa1
Saiful Islam <saifulislamsaif797@gmail.com>
2024-06-07 16:57:56 +0600

    Updated Stdudy sessions details

--- src/Components/StudySessionDetails/StudySessionDetails.jsx
+++ src/Components/StudySessionDetails/StudySessionDetails.jsx
@@ -3,20 +3,23 @@ import { useParams } from 'react-router-dom';
 import useAxiosPublic from '../../Hooks/useAxiosPublic';
 import SectionTitle from '../Shared/SectionTitle';
 import moment from 'moment';
+import useRole from '../../Hooks/useRole';
+import { Tooltip } from 'react-tooltip';
 
 const StudySessionDetails = () => {
     const [session, setSession] = useState( {} );
-
+    const [ remainingDays, setRemainingDays ] = useState(0);
     const {  id } = useParams();
     const axiosPublic = useAxiosPublic();
+    const { role } = useRole();
     
     useEffect(() => {
         axiosPublic.get(`/specific-session/${id}`)
         .then( res => {
             setSession( res.data );
             const remainingDay = parseInt(res.data.regEnd.split('-')[2]) - parseInt(moment().format('L').split('/')[1]) + 1;
-
-            console.log("Remaining Days: ", remainingDay);
+            // console.log("Remaining Days: ", remainingDay);
+            setRemainingDays(remainingDay);
 
         })
     },[])
@@ -26,7 +29,7 @@ const StudySessionDetails = () => {
     const endMonth = parseInt(regEnd?.split('-')[1]);
     const thisMonth = parseInt(moment().format('L').split('/')[0]);
 
-    console.log("End date: ", regEnd, "Today's Date: ", moment().format('L'))
+    // console.log("End date: ", regEnd, "Today's Date: ", moment().format('L'), "Remaining Days: ", remainingDays)
 
     return (
         <div className="w-full mb-5 p-3 bg-teal-50 rounded-xl">
@@ -38,31 +41,94 @@ const StudySessionDetails = () => {
                     <h1 className="text-3xl font-bold text-center mb-5">Course Name: <span className='text-[#00b16e]'>{title}</span></h1>
                     <div className=' md:flex justify-between mx-auto pl-2 md:pl-5 pr-2 md:pr-5'>
                         <div className=''>
-                            <p className="flex gap-5 text-lg text-slate-600 text-justify"><span className='text-slate-400'>Tutor Name:</span>{name}</p>
-                            <p className="flex gap-5 text-lg text-slate-600 text-justify"><span className='text-slate-400'>Registration Start: </span>{ regStart }</p>
-                            <p className="flex gap-5 text-lg text-slate-600 text-justify"><span className='text-slate-400'>Registration End:</span>{regEnd}</p>
-                            <p className="text-lg text-slate-600 text-justify"><span className='text-slate-400 mr-5'>Duration:</span>{duration}</p>
-                            <p className="flex gap-5 text-lg text-slate-600 text-justify"><span className='text-slate-400'>Class Start:</span>{ classStart }</p>
-                            <p className="flex gap-5 text-lg text-slate-600 text-justify"><span className='text-slate-400'>Class End:</span>{ classEnd  }</p>
-                            {/* course registration fee */}
-                            {
-                                regFee === 0 && <p className="flex gap-5 text-lg text-slate-600 text-justify"><span className='text-slate-400'>Registration Fee:</span> Free</p>
-                            }
-                            {
-                                regFee > 0 && <p className="flex gap-5 text-lg text-slate-600 text-justify"><span className='text-slate-400'>Registration Fee:</span> ${regFee}</p>
-                            }
-                            <p className="flex gap-5 text-lg text-slate-600 text-justify"><span className='text-slate-400'>Average Rating:</span>{ 0 }</p>
-                            {/* TODO: rating */}
+                            <div>
+                                <p className="flex gap-5 text-lg text-slate-600 text-justify">
+                                    
+                                    <span className='text-slate-400'>Tutor Name:</span>{name}
+                                </p>
+                                <p className="flex gap-5 text-lg text-slate-600 text-justify">
+
+                                    <span className='text-slate-400'>Registration Start: </span>{ regStart }
+                                </p>
+                                <p className="flex gap-5 text-lg text-slate-600 text-justify">
+                                    
+                                    <span className='text-slate-400'>Registration End:</span>{regEnd}
+                                </p>
+                                <p className="text-lg text-slate-600 text-justify">
+
+                                    <span className='text-slate-400 mr-5'>Duration:</span>{duration}
+
+                                </p>
+                            </div>
+                            <div>
+                                <p className="flex gap-5 text-lg text-slate-600 text-justify">
+                                    
+                                    <span className='text-slate-400'>Class Start:</span>{ classStart }
+                                
+                                </p>
+                                <p className="flex gap-5 text-lg text-slate-600 text-justify">
+                                    
+                                    <span className='text-slate-400'>Class End:</span>{ classEnd  }
+                                
+                                </p>
+                                {/* course registration fee */}
+                                {
+                                    regFee === 0 && <p className="flex gap-5 text-lg text-slate-600 text-justify">
+
+                                        <span className='text-slate-400'>Registration Fee:</span> Free
+
+                                    </p>
+                                }
+                                {
+                                    regFee > 0 && <p className="flex gap-5 text-lg text-slate-600 text-justify">
+                                        
+                                        <span className='text-slate-400'>Registration Fee:</span> ${regFee}
+                                    
+                                    </p>
+                                }
+                                <p className="flex gap-5 text-lg text-slate-600 text-justify">
+                                    {/* TODO: rating */}
+                                    <span className='text-slate-400'>Average Rating:</span>{ 0 }
+                                    
+                                </p>
+                            </div>
                         </div>
                         <div className='md:w-2/3'>
-                            <p className="text-lg text-slate-600 text-justify"><span className='text-slate-400'>Session Details:</span> { description  }</p>
+                            <p className="text-lg text-slate-600 text-justify">
+                                
+                                <span className='text-slate-400'>Session Details:</span> { description  }
+                            
+                            </p>
                         </div>
                     </div>
                     <div>
-                        
+                        {
+                           (role === 'student') && ( remainingDays > 0 ) && ( endMonth <= thisMonth ) && <p 
+                                className="text-lg font-bold cursor-pointer border border-zinc-800 w-1/2 mx-auto flex justify-center items-center mt-5 mb-1 pt-2 pb-2"
+                            >
+                                Book Now
+                            </p>
+                        }
+                        {
+                           (role === 'tutor' || role === 'admin') && ( remainingDays > 0 ) && ( endMonth <= thisMonth ) && <p 
+                                className="text-lg font-bold border border-zinc-800 w-1/2 mx-auto flex justify-center items-center mt-5 mb-1 pt-2 pb-2 disabled cursor-not-allowed"
+                                data-tooltip-id="my-tooltip"
+                                data-tooltip-content={'You do not allow booking!'}
+                            >
+                                Book Now
+                            </p>
+                        }
+                        {
+                            ( endMonth < thisMonth ) && <p 
+                                className="text-lg font-bold cursor-pointer border border-zinc-800 w-1/2 mx-auto flex justify-center items-center mt-5 mb-1 pt-2 pb-2"
+                            >
+                               Registration Closed
+                            </p>
+                        }
                     </div>
                 </div>
             </div>
+            <Tooltip id="my-tooltip" />
         </div>
     );
 };
--- src/Hooks/useRole.jsx
+++ src/Hooks/useRole.jsx
@@ -4,11 +4,11 @@ import useAuth from "./useAuth";
 
 const useRole = () => {
     const { user } = useAuth();
-    const [role, setRole] = useState();
+    const [role, setRole] = useState(' ');
     const axiosPublic = useAxiosPublic();
 
     useEffect(() => {
-        const email = user.email;
+        const email = user?.email;
         axiosPublic.get(`user/role/${email}`)
         .then(res => {
             setRole(res.data.role);
