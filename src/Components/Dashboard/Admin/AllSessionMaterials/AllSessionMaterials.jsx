9dac457d8f167d2240fa1911a33d648f7aa8ea49
Saiful Islam <saifulislamsaif797@gmail.com>
2024-06-09 02:51:45 +0600

    Get all session material data from db

--- src/Components/Dashboard/Admin/AllSessionMaterials/AllSessionMaterials.jsx
+++ src/Components/Dashboard/Admin/AllSessionMaterials/AllSessionMaterials.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../Shared/SectionTitle';
+import { useSearchParams } from 'react-router-dom';
+import useAxiosPublic from '../../../../Hooks/useAxiosPublic';
 
 const AllSessionMaterials = () => {
+    const [ materials, setMaterials ] = useState( [] );
+    const axiosPublic = useAxiosPublic();
+
+    useEffect(() => {
+        axiosPublic.get('/specific-session-material')
+        .then( res => {
+            // console.log(res.data);
+            setMaterials( res.data );
+        })
+    })
     return (
         <div>
             <SectionTitle title='Session Materials'/>
+            <div>
+
+            </div>
         </div>
     );
 };
