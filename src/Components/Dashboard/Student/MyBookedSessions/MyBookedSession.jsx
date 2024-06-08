a2a35e6b333eabf032cb6e4033b8c5d980dbc033
Saiful Islam <saifulislamsaif797@gmail.com>
2024-06-08 11:24:44 +0600

    Updated student booked sessions page

--- src/Components/Dashboard/Student/MyBookedSessions/MyBookedSession.jsx
+++ src/Components/Dashboard/Student/MyBookedSessions/MyBookedSession.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../Shared/SectionTitle';
 import useAxiosPublic from '../../../../Hooks/useAxiosPublic';
 import useAuth from '../../../../Hooks/useAuth';
@@ -8,9 +8,50 @@ const MyBookedSession = () => {
     const { user } = useAuth();
     const [ bookedSessions, setBookedSessions ] = useState( [] );
 
+    useEffect( () => {
+       axiosPublic.get(`student-booked-sessions/${user.email}`)
+       .then( res => {
+            // console.log(res.data);
+            setBookedSessions(res.data);
+       })
+    })
+
     return (
         <div className=''>
             <SectionTitle title='All Booked Sessions'/>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra">
+                        {/* head */}
+                        <thead>
+                            <tr className="text-lg bg-slate-100">
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Session ID</th>
+                                <th>Class Start</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                bookedSessions?.map((session, index) => <tr key={session._id}>
+                                    <th>{index+1}</th>
+                                    <td>{session.title}</td>
+                                    <td>{session.sessionId}</td>
+                                    <td>{session.classStart}</td>
+                                    <td>
+                                        <button 
+                                            className="btn btn-ghost btn-outline btn-sm"
+                                        >
+                                            View Details
+                                        </button>
+                                    </td>
+                                </tr> )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
