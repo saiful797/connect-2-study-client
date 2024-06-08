fd5932b373798d5f9484d0af935452a5528d06c0
Saiful Islam <saifulislamsaif797@gmail.com>
2024-06-08 18:32:53 +0600

    Get uploded materias from db

--- src/Components/Dashboard/Tutor/AllMaterials/AllMaterials.jsx
+++ src/Components/Dashboard/Tutor/AllMaterials/AllMaterials.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../Shared/SectionTitle';
+import useAuth from '../../../../Hooks/useAuth';
+import useAxiosPublic from '../../../../Hooks/useAxiosPublic';
 
 const AllMaterials = () => {
+    const { user } = useAuth();
+    const [ materials, setMaterials ] = useState ( [] );
+    const axiosPublic = useAxiosPublic();
+
+    useEffect( () => {
+        axiosPublic.get(`/study-session-material/${user.email}`)
+        .then( res => {
+            console.log( res.data );
+        })
+    })
     return (
         <div>
             <SectionTitle title='Uploaded Materials'/>
