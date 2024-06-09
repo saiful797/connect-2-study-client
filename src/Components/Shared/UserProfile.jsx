eab1010e53e11d6446bc973260083985ca64cfe3
Saiful Islam <saifulislamsaif797@gmail.com>
2024-06-09 19:06:06 +0600

    Updated profile section

--- src/Components/Shared/UserProfile.jsx
+++ src/Components/Shared/UserProfile.jsx
@@ -6,7 +6,7 @@ const UserProfile = () => {
     const { role } = useRole();
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-green-50">
+        <div className="flex items-center justify-center min-h-screen bg-green-50 rounded-xl">
             <div className="max-w-sm bg-white shadow-lg rounded-xl overflow-hidden transform transition-transform duration-300 hover:scale-105">
                 <div className="relative">
                 <img
@@ -23,9 +23,13 @@ const UserProfile = () => {
                 </div>
                 <div className="text-center p-6">
                     <h1 className="text-2xl font-semibold text-gray-900">{user.displayName}</h1>
-                    <p className="text-sm text-gray-600 mt-1">{role}</p>
+                    <p 
+                        className="text-sm badge badge-outline badge-error text-gray-600 mt-1 "
+                    >
+                        {role}
+                    </p>
                     <p className="mt-4 text-gray-700">
-                        Your act as a { role } here. You can only access your page. 
+                        As a <span className="text-red-500">{ role }</span> you can only access your accessible pages and perform your task. 
                     </p>
                 </div>
             </div>
