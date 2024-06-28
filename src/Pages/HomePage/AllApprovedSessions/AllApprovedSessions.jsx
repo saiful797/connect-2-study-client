c148054855c7d24617c846f14a63b6ced5b01bee
Saiful Islam <saifulislamsaif797@gmail.com>
2024-06-28 08:14:10 +0600

    Updated all courses section for home page

--- src/Pages/HomePage/AllApprovedSessions/AllApprovedSessions.jsx
+++ src/Pages/HomePage/AllApprovedSessions/AllApprovedSessions.jsx
@@ -12,19 +12,19 @@ const AllApprovedSessions = ({ session }) => {
     // console.log(remainingDay)
 
     return (
-        <div className="card bg-white shadow-xl">
+        <div className="card bg-white hover:bg-[#f0e4dd] hover:border hover:border-[#D35400] shadow-xl">
             <div className="card-body">
                 <h2 className="card-title">{ title }</h2>
                 <p className=''><span className='text-lg font-medium text-zinc-900'>Course Details: </span>{ description }</p>
             </div>
             <div className="flex justify-between gap-10 pl-5 pr-10 pb-5 ml-3 -mt-5">
                 {
-                   ( remainingDay > 0 ) && ( endMonth <= thisMonth ) && <p className="bg-stone-200 text-green-600 pl-2 pr-2 w-16 flex justify-center items-center">ongoing</p>
+                   ( remainingDay > 0 ) && ( endMonth <= thisMonth ) && <p className="bg-green-50 text-green-600 pl-2 pr-2 w-16 flex justify-center items-center">ongoing</p>
                 }
                 {
-                   ( remainingDay === 0 ) && ( endMonth < thisMonth ) && <p className="bg-stone-200 text-red-500 pl-2 pr-2 w-16 flex justify-center items-center">closed</p>
+                   ( remainingDay === 0 ) && ( endMonth < thisMonth ) && <p className="bg-red-50 text-red-500 pl-2 pr-2 w-16 flex justify-center items-center">closed</p>
                 }
-                <Link to={`/study-session-details/${session._id}`} className='text-lg font-bold cursor-pointer border border-zinc-800 pl-3 pr-3 hover:bg-green-100 hover:text-[#00b16e]'>
+                <Link to={`/study-session-details/${session._id}`} className='text-lg font-bold cursor-pointer border border-zinc-800 hover:border-[#D35400] pl-3 pr-3 hover:bg-white hover:text-[#D35400]'>
                     Read more
                 </Link>
             </div>
